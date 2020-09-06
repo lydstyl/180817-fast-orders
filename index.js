5c5126a80ec8415b0dd71cf16c2a0d8c440ede81
Gabriel Brun <lydstyl@gmail.com>
2020-09-06 13:54:17 +0200

    change style & precision

--- index.js
+++ index.js
@@ -1,26 +1,161 @@
-let ordersNb = 8;
-let max = 0.3
-let min = 0.1
-let totalToTrade = 0.01;
+const ordersDiv = document.querySelector('.orders')
+const button = document.querySelector('button')
+const sell = document.querySelector('.sell')
+let sumQte = (sumTotal = 0)
 
-let interval = (max - min) / ordersNb;
-let orderPrice = max;
-let orders = [];
-let orderVal = totalToTrade / ordersNb;
-let amount = orderVal / orderPrice
-let total = amount * orderPrice
-let masterTotal = 0;
+const PRECISION = 4
 
-for (let i = 0; i < ordersNb; i++) {
+initValues = {
+  ordersNb: 3,
+  max: 0.00042,
+  min: 0.00037,
+  totalToTrade: 0.2,
+}
+
+/**
+ * Return an array with all buy orders. Each order is an object
+ **/
+function getOrders(ordersNb, max, min, totalToTrade) {
+  let interval = (max - min) / (parseInt(ordersNb, 10) - 1)
+  let orderPrice = max
+  let orders = []
+  let orderVal = totalToTrade / ordersNb
+  let amount = orderVal / orderPrice
+  let total = amount * orderPrice
+  let masterTotal = 0
+  for (let i = 0; i < parseInt(ordersNb, 10); i++) {
     orders.push({
-        'price' : orderPrice.toFixed(7),
-        'amount' : amount.toFixed(5),
-        'total' : total.toFixed(5)
-    });
-    orderPrice -= interval;
+      price: parseFloat(parseFloat(orderPrice).toPrecision(PRECISION)),
+      amount: parseFloat(amount).toPrecision(PRECISION),
+      total: parseFloat(total).toPrecision(PRECISION),
+    })
+    orderPrice -= interval
     amount = orderVal / orderPrice
     total = amount * orderPrice
-    masterTotal += total;
+    masterTotal += total
+  }
+  return orders
 }
-console.log(orders);
-console.log(masterTotal);
+
+/**
+ * Return an array with all sell orders. Each order is an object
+ **/
+function getOrdersSell(ordersNb, max, min, totalToTrade) {
+  let interval = (max - min) / (parseInt(ordersNb, 10) - 1)
+  let orderPrice = parseFloat(min)
+  let orders = []
+  let amount = totalToTrade / ordersNb
+  let total = amount * orderPrice
+
+  for (let i = -1; i < parseInt(ordersNb, 10) - 1; i++) {
+    orders.push({
+      price: parseFloat(orderPrice).toPrecision(PRECISION),
+      amount: parseFloat(amount).toPrecision(PRECISION),
+      total: parseFloat(total).toPrecision(PRECISION),
+    })
+    orderPrice += interval
+    total = amount * orderPrice
+    //masterTotal += total;
+  }
+  return orders
+}
+
+/**
+ * Create a tag with a buy or sell order and append it to the orders div
+ **/
+function appendOrder(price, amount, total, sum) {
+  let order = document.createElement('div')
+  order.className = 'mt-3 order'
+
+  if (sum) {
+    order.innerHTML = `<span>${price}</span><span>qté ${amount}</span><span>total ${total}</span>`
+  } else {
+    order.innerHTML = `<span>prix </span> <span class="bg-danger" >${price}</span> <span>qté </span>  <span class="bg-info" >${amount}</span> <span>total </span> <span class="bg-warning" >${total}</span>`
+  }
+
+  ordersDiv.appendChild(order)
+}
+
+/**
+ * Show price, quantity and total of each buy order
+ **/
+function showResult() {
+  const ordersNb = document.getElementById('ordersNb').value
+  const max = document.getElementById('max').value
+  const min = document.getElementById('min').value
+  const totalToTrade = document.getElementById('totalToTrade').value
+
+  ordersDiv.innerHTML = ''
+  let orders = getOrders(ordersNb, max, min, totalToTrade)
+  let h2 = document.createElement('h2')
+  h2.innerHTML = 'Les ordres'
+
+  ordersDiv.appendChild(h2)
+
+  // fix asked by Antoine Bonduelle 2020 - 07
+  orders.sort((a, b) => {
+    if (a.price > b.price) {
+      return 1
+    } else {
+      return -1
+    }
+  })
+
+  orders.forEach((o) => {
+    appendOrder(o.price, o.amount, o.total)
+    sumQte += parseFloat(o.amount)
+    sumTotal += parseFloat(o.total)
+  })
+
+  appendOrder(
+    '___TOTAL___ ',
+    sumQte.toPrecision(PRECISION),
+    sumTotal.toPrecision(PRECISION),
+    true
+  )
+  sumQte = sumTotal = 0
+}
+
+/**
+ * Show price, quantity and total of each sell order
+ **/
+function showSell() {
+  const ordersNb = document.getElementById('ordersNb').value
+  const max = document.getElementById('max').value
+  const min = document.getElementById('min').value
+  const totalToTrade = document.getElementById('totalToTrade').value
+
+  ordersDiv.innerHTML = ''
+  let orders = getOrdersSell(ordersNb, max, min, totalToTrade) //
+  let h2 = document.createElement('h2')
+  h2.innerHTML = 'Les ordres'
+  ordersDiv.appendChild(h2)
+
+  orders.forEach((o) => {
+    appendOrder(o.price, o.amount, o.total)
+    sumQte += parseFloat(o.amount)
+    sumTotal += parseFloat(o.total)
+  })
+
+  appendOrder('___TOTAL___ ', sumQte.toFixed(5), sumTotal.toFixed(5), true)
+  sumQte = sumTotal = 0
+}
+
+/**
+ * Initialize the form with values
+ *
+ * @param {Object} values - The input number values : ordersNb, max, min, totalToTrade.
+ */
+function initialize(vals) {
+  const { ordersNb, max, min, totalToTrade } = vals
+
+  document.querySelectorAll('[type=number]').forEach((input) => {
+    input.value = vals[input.id]
+  })
+}
+
+initialize(initValues)
+
+button.addEventListener('click', showResult, false)
+
+sell.addEventListener('click', showSell, false)
